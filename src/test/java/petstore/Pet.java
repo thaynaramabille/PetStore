74dc7d37f8c12b661735cf97a9c142720a3261e4
thaynaramabille-docnix <thaynara.ribeiro@grupoitss.com.br>
2021-08-15 13:21:26 -0300

    Completando o script alterarPet (Metodo PUT)

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -16,9 +16,9 @@ import static org.hamcrest.Matchers.contains;
 // 3 - Classes
 public class Pet {
     // 3.1 - Atributos
-    String uri = "https://petstore.swagger.io/v2/pet"; //endereÃ§o da entidade pet
+    String uri = "https://petstore.swagger.io/v2/pet"; //endereço da entidade pet
 
-    // 3.2 - MÃ©todos e FunÃ§Ãµes
+    // 3.2 - Métodos e Funções
     public String lerJson(String caminhoJson) throws IOException {  //le o arquivo de onde ele vai tirar os dados
         return new String(Files.readAllBytes(Paths.get(caminhoJson)));
     }
@@ -26,27 +26,27 @@ public class Pet {
     // Incluir - Create - Post
 
     // uma classe void(nao retorna nada) executa sem retorno
-    @Test(priority = 1) //identifica o mÃ©todo ou funÃ§Ã£o como um teste para o TestNG
+    @Test(priority = 1) //identifica o método ou função como um teste para o TestNG
     public void incluirPet() throws IOException {
-        String jsonBody = lerJson("dbJson/pet1.json"); //chama a funÃ§Ã£o que vai ler, passando o caminho do arquivo
+        String jsonBody = lerJson("dbJson/pet1.json"); //chama a função que vai ler, passando o caminho do arquivo
 
         // Sintaxe Gherkin
-        // Dado - Quando - EntÃ£o
+        // Dado - Quando - Então
         // Given - When - Then
 
-        given() //dado - prÃ© condiÃ§Ã£o
+        given() //dado - pré condição
                 .contentType("application/json") //comum em API REST - antigas era "text/xml"
                 .log().all() //pede pra logar - envio - ida
-                .body(jsonBody) //fala a informaÃ§Ã£o que vai ser transmitida
+                .body(jsonBody) //fala a informação que vai ser transmitida
         .when() //quando
                 .post(uri)
         .then() //entao
                 .log().all() //volta
-                .statusCode(200) //verificar se a transaÃ§Ã£o foi e voltou - so significa que a mensagem foi e voltou
+                .statusCode(200) //verificar se a transação foi e voltou - so significa que a mensagem foi e voltou
                 .body("name",  is("Maggie"))
                 .body("status", is("available"))
-                .body("category.name", is("XEKAOI")) //quando tenho informaÃ§Ã£o sem colchetes, eu posso utilizar o "is"
-                .body("tags.name", contains("sta")) //se tiver informaÃ§Ã£o dentro de colchetes, entao utilizar "contains"
+                .body("category.name", is("XEKAOI")) //quando tenho informação sem colchetes, eu posso utilizar o "is"
+                .body("tags.name", contains("sta")) //se tiver informação dentro de colchetes, entao utilizar "contains"
         ;
 
     }
@@ -71,8 +71,25 @@ public class Pet {
 
         ;
 
-        System.out.print("O token e " + token);
+        System.out.print("O token é " + token);
 
+    }
+    @Test(priority = 3)
+    public void alterarPet() throws IOException {
+        String jsonbody = lerJson("dbJson/pet2.json");
+
+        given()
+                .contentType("application/json")
+                .log().all()
+                .body(jsonbody)
+        .when()
+                .put(uri)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Maggie"))
+                .body("status", is("sold"))
+        ;
     }
 
 }
